28e6bd2e087d6bb80161b9b8825a1c570008ad8f
ukalto <maxi.gaber@gmail.com>
2021-02-17 11:18:17 +0100

    Watchlist feature

--- animestarter.py
+++ animestarter.py
@@ -1,6 +1,6 @@
 import webbrowser
 
-print(' 1 Finished Animes \n 2 Wait for new episodes \n 3 Episodes coming out steadily \n 4 Episodes existing')
+print(' 1 Finished Animes \n 2 Waiting for new episodes \n 3 Episodes coming out steadily \n 4 Episodes exist \n 5 Watchlist')
 
 number = int(input("Enter a number: "))
 
@@ -10,11 +10,14 @@ if number == 1:
     # Finished Animes
     webbrowser.open('')
 if number == 2:
-    # Wait for new episodes
+    # Waiting for new episodes
     webbrowser.open('')
 if number == 3:
     # Episodes coming out steadily
     webbrowser.open('')
 if number == 4:
-    # Episodes existing
+    # Episodes exist
+    webbrowser.open('')
+if number == 5:
+    # Watchlist
     webbrowser.open('')
